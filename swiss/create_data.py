60223a249adb6d839db95f7964047588e36c4948
Ryan Welch <welchr@umich.edu>
2019-11-01 10:19:16 -0400

    Formatting

--- swiss/create_data.py
+++ swiss/create_data.py
@@ -2,17 +2,17 @@
 
 #===============================================================================
 # Copyright (C) 2016 Ryan Welch, The University of Michigan
-# 
+#
 # This script is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This script is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
@@ -158,7 +158,7 @@ def parse_or_beta(field):
       field = float(field)
     except:
       field = float("nan")
-      
+
     return field
 
 def parse_epacts(v,strict=True):
@@ -298,14 +298,14 @@ def parse_gwas_catalog(filepath,dbpath,outpath):
       trait, rsids, log_pval, or_beta = (e[i] for i in (7,21,28,30))
       author, date, journal = (e[i] for i in (2,3,4))
 
-      # Fix the OR/beta column to be either a float or NA. 
+      # Fix the OR/beta column to be either a float or NA.
       or_beta = parse_or_beta(or_beta)
 
       # Make citation for this entry
       year = parse_year(date)
-      citation = "%s et al. %s %s" % (author,year,journal) 
+      citation = "%s et al. %s %s" % (author,year,journal)
 
-      # Do we have a risk allele for this variant? 
+      # Do we have a risk allele for this variant?
       strongest_snp_and_risk = e[20]
       strongest_snp, risk_allele = parse_strongest_snp_risk(strongest_snp_and_risk)
 
